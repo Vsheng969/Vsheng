f915f42cd4ca114afe0c50ecc592ee0378d350ba
Vsheng969 <479037743@qq.com>
2020-03-27 14:27:13 +0800

    函数

---
+++ 函数的使用/函数的使用/源.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>/*int max(int i, int j)
+{
+	if (j < i)
+		return i;
+	else
+		return j;
+}
+int main(void)
+{
+	int a; int b;
+
+	scanf_s ("%d %d", &a, &b);
+printf("max=%d",	max(a, b));
+return 0;
+}*/
+
+int zhishu(int val);
+int main(void)
+{
+	do {
+		int I;
+		scanf_s("%d", &I);
+		if (zhishu(I) == 1)
+			puts("YES!");
+		else
+			puts("NO!");
+	} while (1);
+	return 0;
+}
+int zhishu(int val)
+{
+	int a;
+	for (a = 2; a < val; ++a)
+	{
+		if (val % a == 0)
+			break;
+	}
+		if (a == val)
+			return 1;
+		else
+			return 0;
+}
